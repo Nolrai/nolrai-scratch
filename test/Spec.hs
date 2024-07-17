599d8bedae10b9e1f0a7b578cc8529608b8974c8
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-07-17 13:17:27 -0700

    WIP;

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,13 @@ module Main (main) where
 
 import Boxes (projectName)
 
-
 main :: IO ()
 main = putStrLn ("Tests for " ++ projectName)
+
+data Coin = H | T
+
+scoreAlice :: List -> Int
+scoreAlice [] = 0
+scoreAlice xs =
+    case take 2 xs of
+    
