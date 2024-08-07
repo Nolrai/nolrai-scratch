3c5aa31e4b6dfb327594413980f22f9a8965eae7
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-08-07 00:33:17 -0700

    I didn't commit the source file before!!

--- app/Main.hs
+++ app/Main.hs
@@ -3,20 +3,39 @@
 
 module Main (main) where
 
+import Control.Lens
 import Control.Monad (guard)
-import Data.ByteString.Char8 qualified as BS
+import Data.ByteString.Lazy.Char8 qualified as BS
+import Data.Set as Set
 import QSpice
 import System.Environment (getArgs)
 import System.Exit (die, exitSuccess)
 import Text.Megaparsec
 import Prelude
+import System.Directory
 
 main :: IO ()
 main = do
   [path] <- getArgs
-  fileContents <- BS.readFile path
-  putStrLn $ "length of file = " <> show (BS.length fileContents)
-  case parse schematicFile path fileContents of
-    Left err -> die $ show err
-    Right raw -> writeFile "out.txt" (show raw)
+  names <- body path
+  putStrLn "Names used:"
+  print `mapM_` getNames raw
   exitSuccess
+
+body path = do
+  isDir <- doesDirectoryExist path
+  if isDir 
+    then foldM (\ acc path -> body path `Set.union` acc) Set.empty =<< listDirectory path 
+    else onSchematicFile path
+
+onSchematicFile path = do
+  fileContents <- BS.readFile path
+  putStrLn $ "length of " <> BS.pack path <> " = " <> show (BS.length fileContents)
+  case parse schematicFile path fileContents of
+    Left err -> do
+      hPutStrLn stderr "Errors:"
+      hPutStrLn stderr $ errorBundlePretty err
+    Right raw -> pure $ getNames raw 
+
+getNames :: Raw -> Set BS.ByteString
+getNames r = (r ^. name) `Set.insert` foldMap getNames (r ^. contents)
---
+++ src/QSpice.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module QSpice where
+
+import Control.Lens
+import Control.Monad (guard)
+import Data.ByteString.Lazy (ByteString, pack)
+import Data.ByteString.Lazy qualified as BS
+import Data.Char (ord)
+import Data.Either (partitionEithers)
+import Data.Functor
+import GHC.Word (Word8)
+import Text.Megaparsec
+import Text.Megaparsec.Byte
+import Text.Megaparsec.Byte.Lexer qualified as L
+
+type Parser = Parsec () ByteString
+
+instance ShowErrorComponent () where
+  showErrorComponent :: () -> String
+  showErrorComponent () = ""
+  errorComponentLen :: () -> Int
+  errorComponentLen () = 0
+
+data Raw = Raw
+  { rawName :: ByteString,
+    rawArgs :: [ByteString],
+    rawProperties :: [Property],
+    rawContents :: [Raw]
+  }
+  deriving (Show)
+
+data Property = Property
+  { propertyName :: ByteString,
+    propertyValue :: ByteString
+  }
+  deriving (Show)
+
+makeFields ''Property
+makeFields ''Raw
+
+schematicFile :: Parser Raw
+schematicFile = string "ÿØÿÛ" *> angleWrap parseRaw
+
+whiteSpace :: Parser ()
+whiteSpace = L.space space1 empty empty
+
+symbol :: ByteString -> Parser ByteString
+symbol = L.symbol whiteSpace
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme whiteSpace
+
+angleOpen, angleClose :: Token ByteString
+angleOpen = 171
+angleClose = 187
+
+isAngleChar :: Word8 -> Bool
+isAngleChar c = (c == angleOpen) || (c == angleClose)
+
+openTag :: Parser ()
+openTag = lexeme (single angleOpen) $> ()
+
+closeTag :: Parser ()
+closeTag = lexeme (single angleClose) $> ()
+
+angleWrap :: Parser a -> Parser a
+angleWrap p = try (openTag *> p <* closeTag)
+
+consP :: Parser Word8 -> Parser ByteString -> Parser ByteString
+start `consP` body = BS.cons <$> start <*> body
+
+name' :: Parser ByteString
+name' = letterChar `consP` (BS.pack <$> some (alphaNumChar :: Parser (Token ByteString)))
+
+data ABC a b c = A a | B b | C c
+
+abcSplit :: [ABC a b c] -> ([a], [b], [c])
+abcSplit [] = ([], [], [])
+abcSplit (x : xs) = f x $ abcSplit xs
+  where
+    f :: ABC a b c -> ([a], [b], [c]) -> ([a], [b], [c])
+    f (A a) = over _1 (a :)
+    f (B b) = over _2 (b :)
+    f (C c) = over _3 (c :)
+
+parseRaw :: Parser Raw
+parseRaw = do
+  n <- parseName
+  a <- argList
+  (p, c) <- angleList
+  pure $ Raw n a p c
+
+parseName :: Parser ByteString
+parseName = label "name" $ lexeme name'
+
+untilAngle :: ParsecT () ByteString Identity (Tokens ByteString)
+untilAngle = takeWhileP (Just "arguments") (not . isAngleChar)
+
+argList :: Parser [ByteString]
+argList = BS.split (fromIntegral $ ord ' ') <$> untilAngle
+
+angleList :: Parser ([Property], [Raw])
+angleList = partitionEithers <$> many angleItem
+
+angleItem = angleWrap ((Left <$> try parseProperty) <|> (Right <$> parseRaw))
+
+parseProperty :: Parser Property
+parseProperty =
+  label "property" $
+    Property
+      <$> parsePropName
+      <*> untilAngle
+
+parsePropName :: Parser ByteString
+parsePropName = lexeme $ name' <* string ":"
