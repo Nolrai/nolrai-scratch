676bdd9aa2202917746aed880d1492807d006d6e
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-08-09 01:45:25 -0700

    WIP

--- src/QSpice.hs
+++ src/QSpice.hs
@@ -9,19 +9,20 @@
 
 module QSpice where
 
+import Control.Concurrent (yield)
 import Control.Lens
 import Control.Monad (guard)
 import Data.ByteString.Lazy (ByteString, pack)
 import Data.ByteString.Lazy qualified as BS
-import Data.Char (ord)
+import Data.Char (chr, isHexDigit, isLetter, ord)
 import Data.Either (partitionEithers)
 import Data.Functor
 import GHC.Word (Word8)
-import Text.Megaparsec
-import Text.Megaparsec.Byte
-import Text.Megaparsec.Byte.Lexer qualified as L
-import Data.Text
 import Linear.V2
+import Text.Megaparsec hiding (Label)
+import Text.Megaparsec.Byte
+import Text.Megaparsec.Byte.Lexer (decimal, float)
+import Text.Megaparsec.Byte.Lexer qualified as L
 
 type Parser = Parsec () ByteString
 
@@ -34,7 +35,6 @@ instance ShowErrorComponent () where
 data Raw = Raw
   { rawName :: ByteString,
     rawArgs :: [ByteString],
-    lable :: Text,
     rawProperties :: [Property],
     rawContents :: [Raw]
   }
@@ -46,6 +46,15 @@ data Property = Property
   }
   deriving (Show)
 
+data Arg
+  = Pos (V2 Double)
+  | Value Double
+  | Label ByteString
+  | Ident ByteString
+  | Image ByteString
+  deriving (Show)
+
+makeClassy ''Arg
 makeFields ''Property
 makeFields ''Raw
 
@@ -53,7 +62,7 @@ schematicFile :: Parser Raw
 schematicFile = string "ÿØÿÛ" *> angleWrap parseRaw
 
 whiteSpace :: Parser ()
-whiteSpace = L.space space1 empty empty
+whiteSpace = L.space space1 Text.Megaparsec.empty Text.Megaparsec.empty
 
 symbol :: ByteString -> Parser ByteString
 symbol = L.symbol whiteSpace
@@ -102,32 +111,55 @@ parseRaw = do
   pure $ Raw n a p c
 
 parseName :: Parser ByteString
-parseName = label "name" $ lexeme name'
+parseName = lexeme name' <?> "name"
 
-argList :: Parser [ByteString]
+argList :: Parser [Arg]
 argList = many (lexeme parseArg)
 
-data Arg 
-  = Pos (V2 Double)
-  | Value Double
-  | Label Text
-  | Ident ByteString
-  | Image ByteString
+parseArg :: Parser Arg
+parseArg = (parseImage <|> parsePos <|> parseValue <|> parseLabel <|> parseIdent) <?> "argument"
 
-parseArg =
-  parsePos <|> parseValue <|> parseLable <|> parse
+parsePos, parseValue, parseLabel, parseImage :: Parser Arg
+parsePos = lexeme $ do
+  symbol "("
+  x <- float
+  symbol ","
+  y <- float
+  symbol ")"
+  pure . Pos $ V2 x y
+parseValue = try . lexeme $ Value <$> float
+parseLabel = lexeme $ do
+  quoteMark
+  str <- takeWhileP Nothing ((/= '"') . toChar)
+  quoteMark
+  pure $ Label str
+
+parseIdent = lexeme $ Ident <$> takeWhile1P (Just "letter") (isLetter . toChar)
+
+parseImage = try . lexeme $ do
+  hexDigits <- takeWhileP (Just "Hex Digit") (isHexDigit . toChar)
+  let n = BS.length hexDigits
+  guard $ (n >= 80) && even n
+  pure $ Image hexDigits
+
+quoteMark :: Parser ()
+quoteMark = symbol "\"" $> ()
 
 angleList :: Parser ([Property], [Raw])
 angleList = partitionEithers <$> many angleItem
 
+angleItem :: Parser (Either Property Raw)
 angleItem = angleWrap ((Left <$> try parseProperty) <|> (Right <$> parseRaw))
 
 parseProperty :: Parser Property
 parseProperty =
-  label "property" $
+  (<?> "property") $
     Property
       <$> parsePropName
-      <*> untilAngle
+      <*> takeWhile1P (Just "property value") (not . isAngleChar)
 
 parsePropName :: Parser ByteString
 parsePropName = lexeme $ name' <* string ":"
+
+toChar :: Word8 -> Char
+toChar = chr . fromIntegral
